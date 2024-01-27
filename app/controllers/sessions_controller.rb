fe73d6eb7ee91ad242e8a73529b31bcd2cc5b18c
Leonidez Acosta <acosta.leonidez@gmail.com>
2024-01-27 17:11:48 -0500

    ensure login form renders login failure result correctly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,26 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
+    existing_user = User.find_by_id(cookies.encrypted[:user_id])
+
+    if existing_user
+      redirect_to votes_new_url
+    else
+      @user_fields = User.new
+    end
   end
 
   def create
-    existing_user = User.find_by(user_params)
-    @user = User.new(user_params)
+    existing_user = User.find_by_email(params[:email])
+    new_user = User.new(user_params)
 
     if existing_user
       cookies.encrypted[:user_id] = existing_user.id
       redirect_to votes_new_url
-    elsif @user.save
-      cookies.encrypted[:user_id] = @user.id
+    elsif new_user.save
+      cookies.encrypted[:user_id] = new_user.id
       redirect_to votes_new_url
     else
+      @user_fields = new_user
       render :new, status: :unprocessable_entity
     end
   end
