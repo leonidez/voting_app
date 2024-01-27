6b5451a353f7d43e9bf4a15a96c39326afb99780
Leonidez Acosta <acosta.leonidez@gmail.com>
2024-01-27 12:50:40 -0500

    redirect to root if user not found for votes#new

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,5 +3,10 @@ class VotesController < ApplicationController
   end
 
   def new
+    @user = User.find_by_id(cookies.encrypted[:user_id])
+
+    if !@user
+      redirect_to root_url
+    end
   end
 end
