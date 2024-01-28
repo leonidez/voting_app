b15095039c1cc0d06fb05017c6383ef12f6436d7
Leonidez Acosta <acosta.leonidez@gmail.com>
2024-01-28 00:31:12 -0500

    set up voting page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,22 @@
+# typed: true
+
 class VotesController < ApplicationController
   def index
   end
 
   def new
-    @user = User.find_by_id(cookies.encrypted[:user_id])
+    @user = User.find_by(id: cookies.encrypted[:user_id])
 
     if !@user
       redirect_to root_url
       flash[:notice] = 'Please log in before continuing.'
-    # elsif @user.has_voted
-    #   redirect_to votes_index_url
+    elsif @user.has_voted?
+      redirect_to votes_url
+      flash[:notice] = 'You have already voted.'
+    else
+      @candidate_fields = Candidate.new
+      @candidates = Candidate.all
+      @expiration = Time.now + 5.minutes
     end
   end
 end
