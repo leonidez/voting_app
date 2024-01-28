f99e7857f4405c6d05fc1c164cf5c38913e44aba
Leonidez Acosta <acosta.leonidez@gmail.com>
2024-01-28 13:17:04 -0500

    wrap vote for candidate in transaction

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -3,11 +3,21 @@
 class CandidatesController < ApplicationController
   def create
     user = User.find_by(id: cookies.encrypted[:user_id])
-    candidate = Candidate.create(candidate_params)
-    vote = Vote.create(user: user, candidate: candidate)
 
-    redirect_to votes_url
-    flash[:notice] = 'Thanks for voting!'
+    if user
+      user.transaction do
+        candidate = Candidate.create!(candidate_params)
+        vote = Vote.create!(user: user, candidate: candidate)
+
+        redirect_to votes_url
+        flash[:notice] = 'Thanks for voting!'
+      rescue ActiveRecord::RecordInvalid
+        @candidate_fields = candidate
+        @candidates = Candidate.all
+        render 'votes/new', status: :unprocessable_entity
+      end
+    end
+
   end
 
   private
