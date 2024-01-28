b25a9ff310ab93fcd310197287b5060222cd9a8b
Leonidez Acosta <acosta.leonidez@gmail.com>
2024-01-28 11:59:53 -0500

    save vote for candidate

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,18 @@
+# typed: true
+
 class CandidatesController < ApplicationController
   def create
+    current_user = User.find_by(id: cookies.encrypted[:user_id])
+    candidate = Candidate.create(candidate_params)
+    vote = Vote.create(user: current_user, candidate: candidate)
+
+    redirect_to votes_url
+    flash[:notice] = 'Thanks for voting!'
   end
+
+  private
+
+    def candidate_params
+      params.require(:candidate).permit(:name)
+    end
 end
