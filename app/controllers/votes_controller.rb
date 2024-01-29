ca31b4346b7d8a1a85824eca0d41e13fa790a0c9
Leonidez Acosta <acosta.leonidez@gmail.com>
2024-01-29 18:06:33 -0500

    cast vote for existing candidate

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -15,9 +15,27 @@ class VotesController < ApplicationController
       redirect_to votes_url
       flash[:notice] = 'You have already voted.'
     else
+      @vote_fields = Vote.new
       @candidate_fields = Candidate.new
       @candidates = Candidate.all
       @expiration = Time.now + 5.minutes
     end
   end
+
+  def create
+    user = User.find_by(id: cookies.encrypted[:user_id])
+    candidate = Candidate.find_by(id: vote_params[:candidate_id])
+
+    if user
+      Vote.create!(user: user, candidate: candidate)
+      redirect_to votes_url
+      flash[:notice] = 'Thank you for voting!'
+    end
+  end
+
+  private
+
+    def vote_params
+      params.require(:vote).permit(:candidate_id)
+    end
 end
