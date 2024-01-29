7d4052dc61163081fadfb1b9b2f7ef590bdcb267
Leonidez Acosta <acosta.leonidez@gmail.com>
2024-01-28 22:01:52 -0500

    enforce candidate limit

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -4,4 +4,13 @@ class Candidate < ApplicationRecord
   validates :name, uniqueness: true
 
   has_many :votes
+
+  before_create :enforce_candidate_limit
+
+  private
+    def enforce_candidate_limit
+      if Candidate.count == 10
+        throw :abort
+      end
+    end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,5 @@
+# typed: true
+
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :candidate
